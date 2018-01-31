5c34bb82070992d3a7dba3152dcc20956448225d
Paul Merritt <pdm309@nyu.edu>
2018-01-30 22:31:36 -0500

    prototype 1

---
+++ Prototype #1/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float jump;
+    private Rigidbody2D rigidbody;
+    private bool alive = true;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (alive)
+        {
+            rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                if (rigidbody.position.y <= -3.7) {
+                    rigidbody.velocity = new Vector2(speed, jump);
+                }
+            }
+        }
+	}
+
+    void OnCollisionEnter2D (Collision2D obj)
+    {
+        if (obj.gameObject.tag == "Enemy")
+        {
+            alive = false;
+        }
+    }
+
+    
+}
---
+++ Prototype #1/Assets/Teleport.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+    public PlayerController player;
+    private float distance;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = player.transform.position.x - transform.position.x;
+        if (distance >= 10)
+        {
+            transform.position = new Vector3(player.transform.position.x + 8, transform.position.y, transform.position.z);
+        }
+    }
+}
