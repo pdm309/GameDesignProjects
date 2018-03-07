184675ba0091b87920d09aeef1b4bd6dd23cfbc4
Paul Merritt <pdm309@nyu.edu>
2018-03-07 00:26:19 -0500

    uploading proto 6

---
+++ Prototype #6/Assets/BulletController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    /*
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+        Collider2D collider = obj.collider;
+        
+        if (obj.gameObject.tag == "ProcGround" || obj.gameObject.tag == "Wall")
+        {
+
+        }
+    }
+    */
+}
---
+++ Prototype #6/Assets/DontDestroyMusic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyMusic : MonoBehaviour
+{
+
+    private static DontDestroyMusic instance = null;
+    public static DontDestroyMusic Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
+ // any other methods you need
+ // any other methods you need
